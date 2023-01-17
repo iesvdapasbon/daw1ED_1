9d1fb530fd7d83572d375a65af6333325f76c303
Pasqual <iesvda.pasbon@gmail.com>
2023-01-17 19:47:27 +0100

    Prueba de push

--- HolaMundoApp.java
+++ HolaMundoApp.java
@@ -5,6 +5,6 @@ class HolaMundoApp {
       public static  void main ( String args[]) 
       {
 	System.out.println ("Hola mundo!");
-        System.out.println ("Hola Git!");
+        System.out.println ("Hola Git!!");
       }
 }
