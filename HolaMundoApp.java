91c9717825f64ec4652b68246a9b716799de5caa
iesvdapasbon <122916005+iesvdapasbon@users.noreply.github.com>
2023-01-17 19:43:07 +0100

    Add files via upload

---
+++ HolaMundoApp.java
@@ -0,0 +1,10 @@
+//Aplicación holamundo de ejemplo
+//
+
+class HolaMundoApp {
+      public static  void main ( String args[]) 
+      {
+	System.out.println ("Hola mundo!");
+        System.out.println ("Hola Git!");
+      }
+}
